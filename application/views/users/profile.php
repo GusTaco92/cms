f46675b859593d5dd0e6fb33484c804c3fd493cb
informaticacolmexkinprim <68747018+informaticacolmexkinprim@users.noreply.github.com>
2020-07-24 11:32:10 -0500

    Se cambio el titulo de usuario en perfil/usuario

--- application/views/users/profile.php
+++ application/views/users/profile.php
@@ -22,7 +22,7 @@
 
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Perfil XXX</h3>
+              <h3 class="box-title">Perfil <?php echo $user_data['username']; ?></h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
